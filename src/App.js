b3edc7b5f5f4b9e8220c9b362f292f5152df5cf4
everywherejacobkim <everywherejacobkim@gmail.com>
2022-08-23 22:49:21 -0700

    Fix: eslint & prettier issue

--- src/App.js
+++ src/App.js
@@ -3,26 +3,21 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-
 function App() {
   return (
     <div className="App">
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home">PetShop</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Item</Nav.Link>
-              <Nav.Link href="#pricing">Cart</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">PetShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Item</Nav.Link>
+            <Nav.Link href="#pricing">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
-      <div className="main-bg">
-        
-        </div>
-
-
+      <div className="main-bg"></div>
     </div>
   );
 }
