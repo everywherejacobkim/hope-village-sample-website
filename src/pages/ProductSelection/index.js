dd1e8e4d82a886c4dd1fb8eb509e45bf36a5296b
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-08-27 01:25:38 -0700

    Fix: replace link tags for fixing rendering errors

--- src/pages/ProductSelection/index.js
+++ src/pages/ProductSelection/index.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 
 const ProductPage = () => {
   return (
     <div className="product-page-menu">
       <Nav className="flex-column">
         <div>
-          <Nav.Link href="/productOil">
+          <Link to="/productOil">
             <p>기름류</p>
             <h1 className="text-lg text-lime-700 font-bold">Oil Products</h1>
-          </Nav.Link>
+          </Link>
         </div>
         <div>
-          <Nav.Link href="/productSeasonal">
+          <Link to="/productSeasonal">
             <p>계절 특산품</p>
             <h1 className="text-lg text-lime-700 font-bold">Seasonal Products</h1>
-          </Nav.Link>
+          </Link>
         </div>
         <div>
-          <Nav.Link href="/productEtc">
+          <Link to="/productEtc">
             <p>기타 상품</p>
             <h1 className="text-lg text-lime-700 font-bold">Other Products</h1>
-          </Nav.Link>
+          </Link>
         </div>
       </Nav>
     </div>
